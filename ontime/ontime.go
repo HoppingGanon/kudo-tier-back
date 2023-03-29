0c7b06f21bbbac3809f633d0045ea7a0c1d0e3d4
HoppingGanon <hoppingganonapp@gmail.com>
2023-03-29 22:53:07 +0900

    [Change] セッション削除の定期処理を外部からキャンセルできるように変更 #15

--- ontime/ontime.go
+++ ontime/ontime.go
@@ -1,12 +1,35 @@
 package ontime
 
 import (
+	"context"
 	db "reviewmakerback/db"
 	"time"
 )
 
-func DeleteTempSession() {
-	for range time.Tick(db.TempSessionAlive * time.Second) {
-		db.ArrangeSession()
+func Start() (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	go ArrangeSession(ctx)
+	return ctx, cancel
+}
+
+func ArrangeSession(ctx context.Context) {
+	// タイマーを設定する
+	ticker := time.NewTicker(db.SessionDelSpan * time.Second)
+
+	// 処理終了時、タイマーを終了する
+	defer ticker.Stop()
+
+	// 最初の一回を実行
+	db.ArrangeSession()
+
+	for {
+		select {
+		case <-ctx.Done():
+			// キャンセルされた場合
+			return
+		case <-ticker.C:
+			// タイマーが周回した際
+			db.ArrangeSession()
+		}
 	}
 }
